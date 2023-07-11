04888ce413e6b7ff6f5b4a14e31b7791a457d0ac
TomGarb <tgarbossa@frba.utn.edu.ar>
2023-07-11 19:20:26 -0300

    Comienzo de parcial alquimia

--- programa.pl
+++ programa.pl
@@ -281,4 +281,11 @@ caminoALaFama(Persona):-
 
 /*------------------------------------------------------Parcial Alquimia------------------------------------------------------*/
 
+herramienta(ana, circulo(50,3)).
+herramienta(ana, cuchara(40)).
+herramienta(beto, circulo(20,1)).
+herramienta(beto, libro(inerte)).
+herramienta(cata, libro(vida)).
+herramienta(cata, circulo(100,5)).
 
+% Punto 1
